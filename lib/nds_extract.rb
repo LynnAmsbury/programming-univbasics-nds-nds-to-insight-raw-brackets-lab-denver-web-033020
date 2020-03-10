0baabc94f6fc0f67af70267d769a3abd8d71de40
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-03-10 19:18:01 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -16,9 +16,9 @@ def directors_totals(nds)
   while directors_index < nds.length do # While loop: while the index for the directors is less than the length of the nds do...
     director_name = nds[directors_index][:name] # director_name = the value at the :name key of the director in the hash of the director
                                                 # at the index of the nds director_index is currently at
-    directors_totals[director_name] = 0 # Counter
-    # how long to loop
-    movies_index = 0
+    directors_totals[director_name] = 0 # Counter for the total movie gross of each director's movies
+    # How long to loop
+    movies_index = 0 # Counter 
     while movies_index < nds[directors_index][:movies].length do
     # get to worldwide_gross
     directors_totals[director_name] += nds[directors_index][:movies][movies_index][:worldwide_gross]
